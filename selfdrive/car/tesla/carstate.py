23d54707ee2175b63a99091e7631675a0f3e7e7e
Carlin Hefner <carlinohefner@gmail.com>
2024-07-20 18:08:18 -0700

    update DI_speed to 100hz

--- selfdrive/car/tesla/carstate.py
+++ selfdrive/car/tesla/carstate.py
@@ -139,7 +139,7 @@ class CarState(CarStateBase):
       ("SCCM_steeringAngleSensor", 100),
       ("DAS_bodyControls", 2),
       ("ID3F5VCFRONT_lighting", 10),
-      ("DI_speed", 10),
+      ("DI_speed", 100),
     ]
 
     return CANParser(DBC[CP.carFingerprint]["pt"], messages, CANBUS.vehicle)
